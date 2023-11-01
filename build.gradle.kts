b32dea925a84ada6eb1f4fe1093857bab61a1b5a
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-11-01 22:06:25 +0100

    Gradle publishing configuration
    (cherry picked from commit fb5c63d2555a24c6f19102b246845954e529bb24)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     java
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("maven-publish")
 }
 
 group = "dev.enginecode.inhouse"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -18,9 +19,43 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.1.5")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks.bootJar.get()) // creates executable jar
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        mavenLocal()
+
+        maven {
+            name = "EngineCodeGitHubPackages"
+            url = uri("https://maven.pkg.github.com/EngineCodeDev/packages")
+            credentials {
+                username = project.findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
